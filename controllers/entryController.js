8174b7e892c864f029e63191476ebd4da5cf76af
unknown <g.143.p.y@gmail.com>
2025-05-08 14:15:09 +0530

    update

--- controllers/entryController.js
+++ controllers/entryController.js
@@ -74,3 +74,99 @@ exports.getRecentEntries = async (req, res) => {
     });
   }
 };
+
+// delete entry
+
+exports.deleteEntry = async (req, res) => {
+  try {
+    const deleteEntry = await Entry.findByIdAndDelete(req.params.id);
+    if (!deleteEntry) {
+      return res.status(404).json({
+        message: "Entry not found ",
+      });
+    }
+    res.status(200).json({
+      message: "Entry deleted successfully",
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      message: "System error",
+      error,
+    });
+  }
+};
+
+// update entry
+
+exports.updateEntry = async (req, res) => {
+  try {
+    const updateEntry = await Entry.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!updateEntry) {
+      return res.status(404).json({
+        message: "Entry not found",
+      });
+    }
+    res.status(200).json({
+      message: "Entry updated",
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      message: "System Error",
+      error,
+    });
+  }
+};
+
+// filter data by date, month , year and plant
+
+exports.filterEntries = async (req, res) => {
+  try {
+    const { plant, year, month, date } = req.query;
+    const filter = {};
+
+    if (plant) {
+      filter.plant = plant;
+    }
+
+    if (year || month || date) {
+      let startDate, endDate;
+      if (year && !month && !date) {
+        startDate = new Date(`${year}-01-01T00:00.000Z`);
+        endDate = new Date(`${year}-12-31T23:59.999Z`);
+      }
+
+      if (year && month && !date) {
+        startDate = new Date(`${year}-${month}-01T00:00.000Z`);
+        endDate = new Date(startDate);
+        endDate.setMonth(endDate.getMonth() + 1);
+      }
+
+      if (year && month && date) {
+        startDate = new Date(`${year}-${month}-${date}T00:00.000Z`);
+        endDate = new Date(startDate);
+
+        endDate.setDate(endDate.getDate() + 1);
+      }
+
+      if (startDate && endDate) {
+        filter.createdAt = { $gte: startDate, $lt: endDate };
+      }
+    }
+
+    const entries = await Entry.find(filter);
+    res.status(200).json({
+      success: true,
+      entries: entries,
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      message: "System error",
+      error,
+    });
+  }
+};
--- routes/entryRoutes.js
+++ routes/entryRoutes.js
@@ -4,10 +4,16 @@ const {
   creteEntry,
   getAllEntries,
   getRecentEntries,
+  filterEntries,
+  updateEntry,
+  deleteEntry,
 } = require("../controllers/entryController");
 
 router.post("/add-entry", creteEntry);
 router.get("/entries", getAllEntries);
 router.get("/recent-entrys", getRecentEntries);
+router.get("/entries-filter", filterEntries);
+router.put("/entries-update/:id", updateEntry);
+router.delete("/entries-delete/:id", deleteEntry);
 
 module.exports = router;
